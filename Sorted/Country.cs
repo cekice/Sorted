a4dcb383736ddc04b79efabe9d766489961fa255
edin.cekic <cekic.edin@hotmail.com>
2018-12-22 17:18:22 +0100

    Commit

---
+++ Sorted/Country.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorted
+{
+    class Country
+    {
+        public string Name { get; set; }
+        public int Population { get; set; }
+
+
+        public Country(string name, int population)
+        {
+            Name = name;
+            Population = population;
+        }
+
+    }
+}
--- Sorted/Program.cs
+++ Sorted/Program.cs
@@ -10,12 +10,27 @@ namespace Sorted
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
+            Country[] countryCollection = { new Country("Afghanistan", 34656032),
+                                        new Country("Austria", 8857960),
+                                        new Country("Brazil", 210147125),
+                                        new Country("Denmark", 5789957),
+                                        new Country("Russia", 144526636),
+                                        new Country("China", 1403500365),
+                                        new Country("Turkey", 80810525),
+                                        new Country("Serbia", 7001444),
+                                        new Country("Iraq", 37202572),
+                                        new Country("San Marino", 33344) };
+
+            var countriesSorted = countryCollection.OrderByDescending(countries => countries.Population);
+
+            foreach (Country country in countriesSorted) {
+
+                Console.WriteLine("Country:  {0} , Population:  {1}", country.Name, country.Population);
+
+            }
+            Console.Write("Press any key to exit...");
             Console.ReadKey();
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
         }
     }
 }
